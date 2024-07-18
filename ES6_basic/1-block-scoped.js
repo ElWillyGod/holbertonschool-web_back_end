c8ca3824acfed255989b3dc887623ca7fd659a86
root <manuelantognazza@gmail.com>
2024-07-18 19:25:21 -0300

    1

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,5 +1,4 @@
-export default function taskBlock( /* eslint-disable-next-line no-unused-vars */ trueOrFalse) {
-
+export default function taskBlock(/* eslint-disable-next-line no-unused-vars */ trueOrFalse) {
   const task = false;
   const task2 = true;
 
