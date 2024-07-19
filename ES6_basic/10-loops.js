00039c550ed6a4c4c6663a56eed0847c6fe024a7
root <manuelantognazza@gmail.com>
2024-07-19 01:03:01 -0300

    10 esto si es js

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,6 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (let i = 0; i < array.length; i++) {
-    array[i] = appendString + array[i];
+    const result = [];
+    for (let value of array) {
+      result.push(appendString + value);
+    }
+  
+    return result;
   }
-  return array;
-}
