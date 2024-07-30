2168dcb31828c4948f115f63be26aea53701fce3
root <manuelantognazza@gmail.com>
2024-07-29 21:00:42 -0300

    6 v3

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,9 +1,9 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
+export default function handleProfileSignup(firstName = '', lastName = '', fileName = '') {
   return Promise.allSettled([
-    uploadPhoto(fileName),
     signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
   ]);
 }
