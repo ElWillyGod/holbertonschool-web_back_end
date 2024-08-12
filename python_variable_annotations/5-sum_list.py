50c9485015caa4f6d98882e04d056d382fb9b793
root <manuelantognazza@gmail.com>
2024-08-12 14:11:12 -0300

    sum

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''
+sumar de todos lo elementos de float de la lista
+'''
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    '''
+    sumar de todos lo elementos de float de la lista
+    '''
+
+    return sum(input_list)
