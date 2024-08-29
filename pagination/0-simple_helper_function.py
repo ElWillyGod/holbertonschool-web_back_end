c828ccb9a76b0810caaba4f362d080269e6c4cb4
root <manuelantognazza@gmail.com>
2024-08-29 12:00:34 -0300

    0

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from typing import Tuple
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    Return a tuple of size two containing a start index
+    """
+    start = (page - 1) * page_size
+    end = page * page_size
+    return (start, end)
