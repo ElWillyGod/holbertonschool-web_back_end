59046e8ac0ff85bf0c9ed9429eac236d7ac39cb8
root <manuelantognazza@gmail.com>
2024-09-22 19:41:42 -0300

    jsjsjs

---
+++ Node_JS_basic/4-http.js
@@ -0,0 +1,15 @@
+const http = require('http');
+const port = 1245;
+
+const server = (req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end("Hello Holberton School!");
+};
+
+const app = http.createServer(server);
+app.listen(port, () => {
+    console.log('...');
+});
+
+
+module.exports = app;
